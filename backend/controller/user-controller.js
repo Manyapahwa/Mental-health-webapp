175ae091cec8c1b3911a2964352fee6528bfcc95
Manya <manyapahea@gmail.com>
2025-01-28 18:25:43 -0800

    fixes

--- backend/controller/user-controller.js
+++ backend/controller/user-controller.js
@@ -1,5 +1,4 @@
 import User from '../models/userModel.js';
-import Journal from '../models/journalModel.js';
 import jwt from 'jsonwebtoken';
 import passport from 'passport';
 import bcrypt from 'bcryptjs';
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -3,13 +3,12 @@ import { userSignup, userLogin, getUsers, deleteUser, updateUser, getUserDetails
 import passport from 'passport';
 import '../config/passportConfig.js';
 import multer from 'multer';
-import { create_journal, getPostsByUsername, update_journal, delete_journal} from '../controller/journal-controller.js';
 import { getAnonymousPosts, createAnonymousPost } from '../controller/anonymous-controller.js';
 import { createMood, getMoods } from '../controller/mood-controller.js'; 
 const router = express.Router();
 import upload from '../multer/multerConfig.js';
 import upload1 from '../multer/multerConfig1.js';
-import { getJournalById } from '../controller/journal-controller.js';
+
 import cors from 'cors';
 
 
@@ -26,11 +25,6 @@ router.get('/anonymousPosts', getAnonymousPosts);
 router.post('/createAnonymousPosts', createAnonymousPost);
 
 
-router.post ('/:username', upload1.single('coverPicture'), create_journal);
-router.get('/:username/journals', getPostsByUsername);
-router.put('/journals/:username/:id', update_journal);
-router.delete('/journal-delete/:username/:id', delete_journal);
-router.get('/:username/:id', getJournalById);
 
 //passport.authenticate('jwt', { session: false })
 
