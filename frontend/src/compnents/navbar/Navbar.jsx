8dfabcee38ec59531e4c7a82e3fd35c5fedd4a9d
Manya <manyapahea@gmail.com>
2025-09-20 00:31:56 +0530

    navbar changed

--- frontend/src/compnents/navbar/Navbar.jsx
+++ frontend/src/compnents/navbar/Navbar.jsx
@@ -26,6 +26,7 @@ const Navbar = () => {
   };
 
   const toggleMobileMenu = () => {
+    console.log('toggleMobileMenu clicked, current state:', mobileMenuOpen);
     setMobileMenuOpen(!mobileMenuOpen);
   };
 
@@ -49,7 +50,7 @@ const Navbar = () => {
   const confirmDelete = async () => {
     try {
       // Call the backend route to delete the user
-      await fetch(`${API_URL}/delete-user/${user}`, {
+      await fetch(${API_URL}/delete-user/${user}, {
         method: 'DELETE',
       });
       // Perform logout after deletion
@@ -90,8 +91,13 @@ const Navbar = () => {
           <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={toggleMobileMenu}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 cursor-pointer hover:bg-gray-100"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                console.log('Hamburger button clicked!');
+                toggleMobileMenu();
+              }}
             >
               <span className="sr-only">Open main menu</span>
               <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
@@ -102,19 +108,19 @@ const Navbar = () => {
           <div className="hidden lg:flex lg:gap-x-12">
             <a 
               href="#"
-              onClick={(e) => handleRestrictedClick(e, `/${user}/mood`)} 
+              onClick={(e) => handleRestrictedClick(e, /${user}/mood)} 
               className="text-sm font-semibold leading-6 text-gray-900">Mood Tracker</a>
             <a 
               href="#"
-              onClick={(e) => handleRestrictedClick(e, `/${user}/therapist`)} 
+              onClick={(e) => handleRestrictedClick(e, /${user}/therapist)} 
               className="text-sm font-semibold leading-7 text-gray-900">AI Therapist</a>
             <a 
               href="#"
-              onClick={(e) => handleRestrictedClick(e, `/${user}/quiz`)} 
+              onClick={(e) => handleRestrictedClick(e, /${user}/quiz)} 
               className="text-sm font-semibold leading-6 text-gray-900">Quiz</a>
             <a 
               href="#"
-              onClick={(e) => handleRestrictedClick(e, `/${user}/anonymoussharing`)} 
+              onClick={(e) => handleRestrictedClick(e, /${user}/anonymoussharing)} 
               className="text-sm font-semibold leading-6 text-gray-900">Anonymous Sharing</a>
             <a href="/aboutus" className="text-sm font-semibold leading-6 text-gray-900">About Us</a>
           </div>
@@ -137,7 +143,7 @@ const Navbar = () => {
                 </div>
                 {dropdownOpen && (
                   <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
-                    <a href={`/${user}/profile`} className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
+                    <a href={/${user}/profile} className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
                     <a onClick={(e) => handleLogout(e)} href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">Sign out</a>
                     <a onClick={handleDelete} href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">Delete Profile</a>
                   </div>
@@ -150,7 +156,117 @@ const Navbar = () => {
             )}
           </div>
         </nav>
+        
       </header>
+      
+      {/* Mobile menu */}
+      {mobileMenuOpen && (console.log('Mobile menu should be visible now') ||
+        <div className="lg:hidden">
+          <div className="fixed inset-0 z-[60] bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between">
+                <a href="/" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                  <img className="h-8 w-auto" src="/images/meditation.jpg" alt="Your Company" />
+                </a>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  onClick={toggleMobileMenu}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    <a 
+                      href="#"
+                      onClick={(e) => {
+                        handleRestrictedClick(e, /${user}/mood);
+                        setMobileMenuOpen(false);
+                      }} 
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      Mood Tracker
+                    </a>
+                    <a 
+                      href="#"
+                      onClick={(e) => {
+                        handleRestrictedClick(e, /${user}/therapist);
+                        setMobileMenuOpen(false);
+                      }} 
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      AI Therapist
+                    </a>
+                    <a 
+                      href="#"
+                      onClick={(e) => {
+                        handleRestrictedClick(e, /${user}/quiz);
+                        setMobileMenuOpen(false);
+                      }} 
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      Quiz
+                    </a>
+                    <a 
+                      href="#"
+                      onClick={(e) => {
+                        handleRestrictedClick(e, /${user}/anonymoussharing);
+                        setMobileMenuOpen(false);
+                      }} 
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      Anonymous Sharing
+                    </a>
+                    <a 
+                      href="/aboutus" 
+                      onClick={() => setMobileMenuOpen(false)}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      About Us
+                    </a>
+                  </div>
+                  <div className="py-6">
+                    {isLoggedIn ? (
+                      <div className="space-y-2">
+                        <a 
+                          href={/${user}/profile}
+                          onClick={() => setMobileMenuOpen(false)}
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                          Your Profile
+                        </a>
+                        <a 
+                          href="#"
+                          onClick={(e) => {
+                            handleLogout(e);
+                            setMobileMenuOpen(false);
+                          }}
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                          Sign out
+                        </a>
+                        <a 
+                          href="#"
+                          onClick={() => {
+                            handleDelete();
+                            setMobileMenuOpen(false);
+                          }}
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-red-600 hover:bg-gray-50">
+                          Delete Profile
+                        </a>
+                      </div>
+                    ) : (
+                      <a 
+                        href="/login"
+                        onClick={() => setMobileMenuOpen(false)}
+                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                        Login
+                      </a>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       {/* Delete Profile Modal */}
       {showDeleteModal && (
         <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
